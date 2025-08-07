de40effec2fc1c107dfff0ddb0ccf6accdc7a88c
HP <info@gyld.org>
2025-08-07 17:31:16 -0400

    E2E Jest: switch to jest-expo preset and allow RN/Expo node_modules via transformIgnorePatterns

--- e2e/jest.config.e2e.js
+++ e2e/jest.config.e2e.js
@@ -5,10 +5,14 @@
 module.exports = {
   // Ensure <rootDir> resolves to the project root (one level up from this file)
   rootDir: '..',
-  preset: 'react-native',
+  // Use expo-aware preset so RN/Expo modules in node_modules are transpiled
+  preset: 'jest-expo',
   testEnvironment: 'detox/runners/jest/testEnvironment',
   testMatch: ['<rootDir>/e2e/**/*.e2e.js'],
   setupFilesAfterEnv: ['<rootDir>/e2e/init.js'],
   reporters: ['detox/runners/jest/reporter'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|expo(nent)?|@expo|expo-.*|react-native-.*|@react-native-.*)'
+  ],
 };
 
